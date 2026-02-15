28b0aa18f4dd25f97c76f8242e18d8cf5d2b3f85
SURESHDOMA <sureshsurya33086@gmail.com>
2026-02-15 11:38:05 +0530

    update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main() 
 {
-    std::cout<<"Suresh Welcome to VS Code!";
+    std::cout<<"Suresh Welcome to VS Code!"<<std::endl;
+    std::cout << "Run the CPP File";
     return 0;
 }   
